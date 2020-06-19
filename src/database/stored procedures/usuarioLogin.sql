940039f6bc9d4035db9ed8f407af6b99cdfe9941
francomercado93 <mercadofranco.a@gmail.com>
2020-06-19 20:21:14 -0300

    Fix de sp

--- src/database/stored procedures/usuarioLogin.sql
+++ src/database/stored procedures/usuarioLogin.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `usuarioLogin`(IN usuario VARCHAR(45), IN password VARCHAR(45))
 BEGIN
-	SELECT u.usuario, u.alias, u.nombre, u.apellido, u.password, u.es_vidente as esVidente, tu.tipo_usuario as tipoUsuario, p.nombre as provincia , u.ciudad 
+	SELECT u.usuario, u.alias, u.nombre, u.apellido, u.password, IF(u.es_vidente, 'true', 'false') as esVidente, tu.tipo_usuario as tipoUsuario, p.nombre as provincia , u.ciudad 
 	FROM usuario u
 	INNER JOIN tipo_usuario tu ON tu.id_tipo_usuario = u.id_tipo_usuario
 	INNER JOIN provincia p ON p.id_provincia = u.id_provincia
